25744e6dbb0ad84e86a296aa6b5acffca78fedf2
thisisaciidfree <plyusninavaleria@gmail.com>
2025-09-16 17:42:04 +0300

    Add Startup.cs

---
+++ src/ErrorPulseApi/Startup.cs
@@ -0,0 +1,15 @@
+namespace ErrorPulseApi;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+}
