f2c6ba1568e83c81b69670fd199210d994ec5f1f
thisisaciidfree <plyusninavaleria@gmail.com>
2025-09-18 18:38:51 +0300

    Ускорение работы за счет считывания файла всего один раз

--- src/ErrorPulseApi/Services/ErrorAnalyticsService.cs
+++ src/ErrorPulseApi/Services/ErrorAnalyticsService.cs
@@ -1,6 +1,5 @@
 using System.Collections.Concurrent;
 using System.Globalization;
-using System.Threading.Channels;
 using CsvHelper;
 using CsvHelper.Configuration;
 using ErrorPulseApi.Configuration;
@@ -19,12 +18,11 @@ public class ErrorAnalyticsService : IErrorAnalyticsService
 {
     private readonly DataFoldersOptions _dataFoldersOptions;
     private readonly ILogger<ErrorAnalyticsService> _logger;
-    
+
     private readonly ConcurrentDictionary<string, int> _errorBySeverity;
     private readonly ConcurrentDictionary<string, int> _errorByProductVersion;
     private readonly ConcurrentDictionary<string, int> _topErrorCodes;
-    private readonly ConcurrentDictionary<string, Dictionary<int,int>> _topErrorCodesByHour;
-
+    private readonly ConcurrentDictionary<string, Dictionary<int, int>> _topErrorCodesByHour;
 
     public ErrorAnalyticsService(
         IOptions<DataFoldersOptions> dataFoldersOptions,
@@ -33,10 +31,10 @@ public class ErrorAnalyticsService : IErrorAnalyticsService
         _errorBySeverity = new ConcurrentDictionary<string, int>();
         _errorByProductVersion = new ConcurrentDictionary<string, int>();
         _topErrorCodes = new ConcurrentDictionary<string, int>();
-        _topErrorCodesByHour = new ConcurrentDictionary<string, Dictionary<int, int>>();   
-        
+        _topErrorCodesByHour = new ConcurrentDictionary<string, Dictionary<int, int>>();
+
         _dataFoldersOptions = dataFoldersOptions.Value;
-        _logger = logger;   
+        _logger = logger;
     }
 
     public async Task<bool> CreateAnalyticsFiles()
@@ -44,7 +42,7 @@ public class ErrorAnalyticsService : IErrorAnalyticsService
         var filePath = GetCsvFilePath();
         if (filePath == null) return false;
 
-        await ProcessCsvFileParallel(filePath);
+        await ProcessCsvFile(filePath);
 
         await SaveAnalytics();
 
@@ -71,7 +69,7 @@ public class ErrorAnalyticsService : IErrorAnalyticsService
         return filePath;
     }
 
-    private async Task ProcessCsvFileParallel(string filePath)
+    private async Task ProcessCsvFile(string filePath)
     {
         var config = new CsvConfiguration(CultureInfo.InvariantCulture)
         {
@@ -80,98 +78,42 @@ public class ErrorAnalyticsService : IErrorAnalyticsService
             HeaderValidated = null
         };
 
-        var severityChannel = Channel.CreateUnbounded<ErrorInfo>();
-        var productChannel = Channel.CreateUnbounded<ErrorInfo>();
-        var topErrorChannel = Channel.CreateUnbounded<ErrorInfo>();
-        var topErrorByHourChannel = Channel.CreateUnbounded<ErrorInfo>();
-
-        var severityTask = AggregateSeverity(severityChannel);
-        var productTask = AggregateProductVersion(productChannel);
-        var topErrorTask = AggregateTopErrors(topErrorChannel);
-        var topErrorByHourTask = AggregateTopErrorsByHour(topErrorByHourChannel);
-        
         using var reader = new StreamReader(filePath);
         using var csv = new CsvReader(reader, config);
 
         await foreach (var record in csv.GetRecordsAsync<ErrorInfo>())
         {
-            await severityChannel.Writer.WriteAsync(record);
-            await productChannel.Writer.WriteAsync(record);
-            await topErrorChannel.Writer.WriteAsync(record);
-            await topErrorByHourChannel.Writer.WriteAsync(record);  
-        }
-        
-        severityChannel.Writer.Complete();
-        productChannel.Writer.Complete();
-        topErrorChannel.Writer.Complete();
-        topErrorByHourChannel.Writer.Complete();    
-        
-        await Task.WhenAll(severityTask, productTask, topErrorTask, topErrorByHourTask);
-    }
-    
-    private async Task AggregateSeverity(Channel<ErrorInfo> channel)
-    {
-        await foreach (var record in channel.Reader.ReadAllAsync())
-        {
             _errorBySeverity.AddOrUpdate(record.Severity, 1, (_, old) => old + 1);
-        }
-    }
-
-    private async Task AggregateProductVersion(Channel<ErrorInfo> channel)
-    {
-        await foreach (var record in channel.Reader.ReadAllAsync())
-        {
-            var key = $"{record.Product}_{record.Version}";
-            _errorByProductVersion.AddOrUpdate(key, 1, (_, old) => old + 1);
-        }
-    }
-
-    private async Task AggregateTopErrors(Channel<ErrorInfo> channel)
-    {
-        await foreach (var record in channel.Reader.ReadAllAsync())
-        {
-            var key = $"{record.Product}_{record.Severity}_{record.ErrorCode}";
-            _topErrorCodes.AddOrUpdate(key, 1, (_, old) => old + 1);
-        }
-    }
-    
-    private async Task AggregateTopErrorsByHour(Channel<ErrorInfo> channel)
-    {
-        await foreach (var record in channel.Reader.ReadAllAsync())
-        {
-            var hour = ((DateTime)record.Timestamp).Hour;
             
-            var key = $"{record.Product}_{record.Severity}_{record.ErrorCode}";
-            var innerDict = _topErrorCodesByHour.GetOrAdd(key, _ => new Dictionary<int, int>());
-
-            lock(innerDict)
+            var productKey = $"{record.Product}_{record.Version}";
+            _errorByProductVersion.AddOrUpdate(productKey, 1, (_, old) => old + 1);
+      
+            var errorKey = $"{record.Product}_{record.Severity}_{record.ErrorCode}";
+            _topErrorCodes.AddOrUpdate(errorKey, 1, (_, old) => old + 1);
+            
+            var hour = ((DateTime)record.Timestamp).Hour;
+            var innerDict = _topErrorCodesByHour.GetOrAdd(errorKey, _ => new Dictionary<int, int>());
+            lock (innerDict)
             {
                 if (innerDict.ContainsKey(hour))
-                {
                     innerDict[hour]++;
-                }
                 else
-                {
-                    innerDict[hour] = 1; 
-                }
+                    innerDict[hour] = 1;
             }
         }
     }
-    
+
     private async Task SaveAnalytics()
     {
         await SaveSeverityResults(AnalyticsFileNames.ErrorBySeverityName, _errorBySeverity);
-        await SavProductsWithVersionResults(AnalyticsFileNames.ErrorByProductVersionName, _errorByProductVersion);
+        await SaveProductsWithVersionResults(AnalyticsFileNames.ErrorByProductVersionName, _errorByProductVersion);
         await SaveTopErrorResults(AnalyticsFileNames.TopErrorCodesName, _topErrorCodes);
         await SaveTopErrorByHourResults(AnalyticsFileNames.TopErrorByHourCodesName, _topErrorCodesByHour);
     }
-    
-    private async Task SaveSeverityResults(
-        string fileName,
-        ConcurrentDictionary<string, int> data)
+
+    private async Task SaveSeverityResults(string fileName, ConcurrentDictionary<string, int> data)
     {
         var path = GetPath(fileName);
-
         await using var writer = new StreamWriter(path);
         await writer.WriteLineAsync("Severity,Count");
 
@@ -179,90 +121,73 @@ public class ErrorAnalyticsService : IErrorAnalyticsService
         {
             await writer.WriteLineAsync($"{kvp.Key},{kvp.Value}");
         }
-        
+
         _logger.LogInformation($"CSV analytics {fileName} saved at path: {path}.");
     }
-    
-    private async Task SavProductsWithVersionResults(
-        string fileName,
-        ConcurrentDictionary<string, int> data)
+
+    private async Task SaveProductsWithVersionResults(string fileName, ConcurrentDictionary<string, int> data)
     {
         var path = GetPath(fileName);
-
         await using var writer = new StreamWriter(path);
-        await writer.WriteLineAsync("Products,Version,Count");
+        await writer.WriteLineAsync("Product,Version,Count");
 
         foreach (var kvp in data.OrderByDescending(x => x.Value))
         {
-            var parts = kvp.Key.Split('_'); 
+            var parts = kvp.Key.Split('_');
             await writer.WriteLineAsync($"{parts[0]},{parts[1]},{kvp.Value}");
         }
-        
+
         _logger.LogInformation($"CSV analytics {fileName} saved at path: {path}.");
     }
 
-    private async Task SaveTopErrorResults(
-        string fileName, 
-        ConcurrentDictionary<string,int> data)
+    private async Task SaveTopErrorResults(string fileName, ConcurrentDictionary<string, int> data)
     {
         var path = GetPath(fileName);
-        
-        var topErrorCodes = _topErrorCodes
-            .OrderByDescending(kv => kv.Value)
-            .Take(10)
-            .ToList();
+        var topErrors = _topErrorCodes.OrderByDescending(kv => kv.Value).Take(10).ToList();
 
         await using var writer = new StreamWriter(path);
         await writer.WriteLineAsync("Product,Severity,ErrorCode,Count");
-        
-        foreach (var kvp in topErrorCodes)
+
+        foreach (var kvp in topErrors)
         {
-            var parts = kvp.Key.Split('_'); 
+            var parts = kvp.Key.Split('_');
             await writer.WriteLineAsync($"{parts[0]},{parts[1]},{parts[2]},{kvp.Value}");
         }
-        
-        _logger.LogInformation($"CSV analytics {fileName} saved to path: {path}.");
+
+        _logger.LogInformation($"CSV analytics {fileName} saved at path: {path}.");
     }
-    
-    private async Task SaveTopErrorByHourResults(
-        string fileName, 
-        ConcurrentDictionary<string, Dictionary<int, int>> data)
+
+    private async Task SaveTopErrorByHourResults(string fileName, ConcurrentDictionary<string, Dictionary<int, int>> data)
     {
         var path = GetPath(fileName);
         await using var writer = new StreamWriter(path);
         await writer.WriteLineAsync("Period,Product,Severity,ErrorCode,Count");
-        
-        var result = data
-            .ToArray() 
+
+        var result = data.ToArray()
             .Select(kvp =>
             {
                 var key = kvp.Key;
                 var hourly = kvp.Value;
                 var best = hourly.Aggregate((max, cur) => cur.Value > max.Value ? cur : max);
                 var periodLabel = $"{best.Key:00}:00 - {(best.Key + 1) % 24:00}:00";
-
                 return (Key: key, Hour: periodLabel, Total: best.Value);
-
-                return (Key: key, Hour: $"{best.Key:00}:00 - {(best.Key + 1):00}:00", Total: best.Value);
             })
-            .OrderByDescending(x => x.Total) 
-            .ToList(); 
-        
+            .OrderByDescending(x => x.Total)
+            .ToList();
+
         foreach (var kvp in result)
         {
-            var parts = kvp.Key.Split('_'); 
+            var parts = kvp.Key.Split('_');
             await writer.WriteLineAsync($"{kvp.Hour},{parts[0]},{parts[1]},{parts[2]},{kvp.Total}");
         }
-        
-        _logger.LogInformation($"CSV analytics {fileName} saved to path: {path}.");
+
+        _logger.LogInformation($"CSV analytics {fileName} saved at path: {path}.");
     }
-    
+
     private string GetPath(string fileName)
     {
         fileName += $"_{DateTime.Now:yyyyMMddHHmmss}.csv";
-        
         Directory.CreateDirectory(_dataFoldersOptions.AnalyticsDirDataPath);
-        var path = Path.Combine(_dataFoldersOptions.AnalyticsDirDataPath, fileName);
-        return path;
+        return Path.Combine(_dataFoldersOptions.AnalyticsDirDataPath, fileName);
     }
 }
